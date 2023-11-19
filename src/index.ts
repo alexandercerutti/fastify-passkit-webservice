e6cf552c9a2a0d252fea643de4c08e8d08d9047e
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-11-20 00:12:23 +0100

    Added barrel files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import type { FastifyRequest } from "fastify";
-
-function getAuthorizationToken(request: FastifyRequest): string | undefined {
-	if (!request.headers["Authorization"]) {
-		return undefined;
-	}
-
-	const [, passAuthorizationToken] = (
-		request.headers["Authorization"] as string
-	).split("\x20");
-
-	if (!passAuthorizationToken?.length) {
-		return undefined;
-	}
-
-	return passAuthorizationToken;
-}
+export * as v1 from "./plugins/v1/index.js";
---
+++ src/plugins/v1/index.ts
@@ -0,0 +1,4 @@
+export * from "./list.js";
+export * from "./log.js";
+export * from "./registration.js";
+export * from "./update.js";
