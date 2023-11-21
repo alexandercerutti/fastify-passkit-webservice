ab7ea1358e0377be1db56ea63eefd1888af3dfd0
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-11-21 11:27:18 +0100

    Added import tests

--- specs/registration.test.mjs
+++ specs/registration.test.mjs
@@ -15,6 +15,40 @@ describe("registration service", () => {
 		fastifyInstance = Fastify();
 	});
 
+	describe("import", async () => {
+		it("should import plugin from direct import", async () => {
+			const { default: directFileImport } = await import(
+				/** this will give error when package itself is not linked through `pnpm test` */
+				"fastify-passkit-webservice/v1/registration.js"
+			);
+
+			strictEqual(typeof directFileImport, "function");
+			strictEqual(directFileImport.name, "registrationPlugin");
+		});
+
+		it("should import plugin from v1 entry point", async () => {
+			const { registrationPlugin } = await import(
+				/** this will give error when package itself is not linked through `pnpm test` */
+				"fastify-passkit-webservice/v1"
+			);
+
+			strictEqual(typeof registrationPlugin, "function");
+			strictEqual(registrationPlugin.name, "registrationPlugin");
+		});
+
+		it("should import v1 plugin from global package entry", async () => {
+			const {
+				v1: { registrationPlugin },
+			} = await import(
+				/** this will give error when package itself is not linked through `pnpm test` */
+				"fastify-passkit-webservice"
+			);
+
+			strictEqual(typeof registrationPlugin, "function");
+			strictEqual(registrationPlugin.name, "registrationPlugin");
+		});
+	});
+
 	it("should throw an error if handlers are not provided", async () => {
 		await rejects(
 			async () => {
--- specs/update.test.mjs
+++ specs/update.test.mjs
@@ -15,6 +15,40 @@ describe("update service", () => {
 		fastifyInstance = Fastify();
 	});
 
+	describe("import", async () => {
+		it("should import plugin from direct import", async () => {
+			const { default: directFileImport } = await import(
+				/** this will give error when package itself is not linked through `pnpm test` */
+				"fastify-passkit-webservice/v1/update.js"
+			);
+
+			strictEqual(typeof directFileImport, "function");
+			strictEqual(directFileImport.name, "updatePlugin");
+		});
+
+		it("should import plugin from v1 entry point", async () => {
+			const { updatePlugin } = await import(
+				/** this will give error when package itself is not linked through `pnpm test` */
+				"fastify-passkit-webservice/v1"
+			);
+
+			strictEqual(typeof updatePlugin, "function");
+			strictEqual(updatePlugin.name, "updatePlugin");
+		});
+
+		it("should import v1 plugin from global package entry", async () => {
+			const {
+				v1: { updatePlugin },
+			} = await import(
+				/** this will give error when package itself is not linked through `pnpm test` */
+				"fastify-passkit-webservice"
+			);
+
+			strictEqual(typeof updatePlugin, "function");
+			strictEqual(updatePlugin.name, "updatePlugin");
+		});
+	});
+
 	it("should throw an error if the handler is not provided", async () => {
 		await rejects(
 			async () => {
